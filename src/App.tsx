c55c089a3c11ae7163318ab8bc08b821d040022a
Adam Chernitsky <adamlee2388@gmail.com>
2026-04-27 17:50:35 -0600

    Refactor charts section into component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import UploadSection from "./components/UploadSection";
 import SearchBar from "./components/SearchBar";
 import SummaryCards from "./components/SummaryCards";
 import ExportButtons from "./components/ExportButtons";
+import ChartsSection from "./components/ChartsSection";
 
 import {
   ResponsiveContainer,
@@ -516,80 +517,10 @@ function App() {
 
         {/* Charts */}
         {result && (
-          <>
-            <h2
-              style={{
-                marginTop:
-                  "35px",
-              }}
-            >
-              Visualizations
-            </h2>
-
-            <div
-              style={{
-                display:
-                  "grid",
-                gridTemplateColumns:
-                  "1fr 1fr",
-                gap: "20px",
-              }}
-            >
-              <div
-                style={
-                  cardStyle
-                }
-              >
-                <h3>
-                  Request Totals
-                </h3>
-
-                <ResponsiveContainer
-                  width="100%"
-                  height={260}
-                >
-                  <BarChart
-                    data={
-                      requestChartData
-                    }
-                  >
-                    <CartesianGrid stroke="#334155" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Bar dataKey="requests" />
-                  </BarChart>
-                </ResponsiveContainer>
-              </div>
-
-              <div
-                style={
-                  cardStyle
-                }
-              >
-                <h3>
-                  Failure Rates
-                </h3>
-
-                <ResponsiveContainer
-                  width="100%"
-                  height={260}
-                >
-                  <BarChart
-                    data={
-                      failureChartData
-                    }
-                  >
-                    <CartesianGrid stroke="#334155" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Bar dataKey="rate" />
-                  </BarChart>
-                </ResponsiveContainer>
-              </div>
-            </div>
-          </>
+          <ChartsSection
+            requestChartData={requestChartData}
+            failureChartData={failureChartData}
+          />
         )}
 
         {/* Worst Endpoints */}
