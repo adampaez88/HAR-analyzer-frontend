7f7f56e24ba65fd5dddd5e3d8f7272a81b21fadb
Adam Chernitsky <adamlee2388@gmail.com>
2026-05-12 13:34:55 -0600

    Adding log to help debug issue with deployment

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,11 @@ import type { HarResult } from "../types";
 const BASE_URL =
   import.meta.env.VITE_API_BASE_URL;
 
+console.log(
+  "VITE_API_BASE_URL:",
+  BASE_URL
+);
+
 export const compareHarFiles = async (
   fileA: File,
   fileB: File
